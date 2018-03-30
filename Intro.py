98a42341c38ef7fe9aa893617f94184e220b00be
federicogottardo <federicogottard@gmail.com>
2018-03-30 23:57:47 +0100

    change

--- Intro.py
+++ Intro.py
@@ -1,4 +1,19 @@
 import numpy as np
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
+iris_X = iris.data   # use iris_X.shape --> SHAPE shows the dimensions. it's a property of numpy arrays
+iris_Y = iris.target
+
+np.random.seed(0)
+indices = np.random.permutation(len(iris_X))
+iris_X_train = iris_X[indices[:-10]] # use index -10 as [:-10] to avoid having to specify the length of the array and just select all data from 0 except the last 10
+iris_Y_train = iris_Y[indices[:-10]]
+iris_X_test = iris_X[indices[-10:]]
+iris_Y_test = iris_Y[indices[-10:]]
+
+knn = KNeighborsClassifier()
+knn.fit(iris_X_train,iris_Y_train)
+print(knn.predict(iris_X_test))
+print(iris_Y_test)
